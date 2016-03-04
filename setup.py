89c7086018a184f9df290ebb9190a7d6c52f6740
Spencer Wood <spencercwood@gmail.com>
2016-03-03 20:04:59 -0700

    Removed `twitch watch n`

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name='twinge',
-    version='0.2.1',
+    version='0.2.2',
     author='Spencer Wood',
     author_email='spencercwood@gmail.com',
     description='CLI for Twitch.tv',
