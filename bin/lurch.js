5dc9b0a633b154aa7cf6703d464efebbe9f529a6
Spencer Wood <spencercwood@gmail.com>
2016-09-26 13:02:41 -0600

    Add usage

--- bin/lurch.js
+++ bin/lurch.js
@@ -1,5 +1,15 @@
-#!/usr/bin/env node
-
 import fortyTwo from '../src/something';
 
-console.log(`The meaning of life is ${fortyTwo}`);
+const doc = `Usage:
+  lurch list [--number=<value>]
+  lurch watch <channel> [--quality=<value>]
+  lurch check <channel>
+  lurch (-h | --help | --version)
+
+Options:
+  -h --help          Show this screen.
+  --version          Show version.
+  --number=<value>   Number of streams to list from 1-100. [default: 25]
+  --quality=<value>  Livestreamer quality. [default: best]`
+
+console.log(doc);
