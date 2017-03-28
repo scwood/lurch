6e29ce7089d0361643fbebb6d565db0869a695ea
Spencer Wood <spencercwood@gmail.com>
2017-03-27 19:19:51 -0600

    Include src files with distribution, stop using class in Twitch file

--- src/twitch.js
+++ src/twitch.js
@@ -1,42 +1,37 @@
 import fetch from 'isomorphic-fetch';
 import querystring from 'querystring';
 
-class Twitch {
-
-  constructor() {
-    this.baseUrl = 'https://api.twitch.tv/kraken';
-    this.headers = {
-      accept: 'application/vnd.twitchtv.v5+json',
-      'client-id': '2n7irufqjtyyayigyc4ubzq174axeex',
-    };
-  }
-
-  async getStreams(limit = null, game = null) {
-    let url = `${this.baseUrl}/streams?`;
-    const queryParams = {};
-    if (limit) {
-      queryParams.limit = limit;
-    }
-    if (game) {
-      queryParams.game = game;
-    }
-    url += querystring.stringify(queryParams);
-    const { streams } = await this.fetchJsonWithHeaders(url);
-    return streams;
-  }
-
-  async searchStreams(query) {
-    let url = `${this.baseUrl}/search/streams?`;
-    url += querystring.stringify({ limit: 100, query });
-    const { streams } = await this.fetchJsonWithHeaders(url);
-    return streams;
-  }
-
-  async fetchJsonWithHeaders(url, config = {}) {
-    config.headers = Object.assign({}, config.headers, this.headers);
-    const result = await fetch(url, config);
-    return await result.json();
-  }
+const baseUrl = 'https://api.twitch.tv/kraken';
+const headers = {
+  accept: 'application/vnd.twitchtv.v5+json',
+  'client-id': '2n7irufqjtyyayigyc4ubzq174axeex',
 }
 
-export default new Twitch();
+async function getStreams(limit = null, game = null) {
+  let url = `${baseUrl}/streams?`;
+  const queryParams = {};
+  if (limit) {
+    queryParams.limit = limit;
+  }
+  if (game) {
+    queryParams.game = game;
+  }
+  url += querystring.stringify(queryParams);
+  const { streams } = await fetchJsonWithHeaders(url);
+  return streams;
+}
+
+async function searchStreams(query) {
+  let url = `${baseUrl}/search/streams?`;
+  url += querystring.stringify({ limit: 100, query });
+  const { streams } = await fetchJsonWithHeaders(url);
+  return streams;
+}
+
+async function fetchJsonWithHeaders(url, config = {}) {
+  config.headers = Object.assign({}, config.headers, headers);
+  const result = await fetch(url, config);
+  return await result.json();
+}
+
+export default { getStreams, searchStreams };
